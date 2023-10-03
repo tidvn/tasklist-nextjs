fd67ed77e70296d73e3347fc74ae6d319bdce2c0
phung <you@example.com>
2023-10-03 23:36:50 +0700

    update

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -4,8 +4,8 @@ export function middleware(request: NextRequest) {
     const origin =request.headers.get("origin")
 
     const res = NextResponse.next()
-    res.headers.append('Access-Control-Allow-Credentials', "false")
-    res.headers.append('Access-Control-Allow-Origin', origin || '*')
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Origin', 'http://localhost:3000' || origin || '*')
     res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,POST,PUT')
     res.headers.append('Access-Control-Allow-Headers',
         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
