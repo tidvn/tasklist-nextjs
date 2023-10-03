be946451c4a4331df0d4558b2b27f4f6931e81b6
phung <you@example.com>
2023-10-03 22:24:59 +0700

    update cors

--- src/app/api/task/[id]/route.ts
+++ src/app/api/task/[id]/route.ts
@@ -2,67 +2,46 @@ import clientPromise from "@/database/mongodb";
 import { Task } from "@/types/index";
 import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
-
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
-  const { id } = params
+const  headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Access-Control-Allow-Credentials': 'true',
+}
+export async function PUT(req: Request,{ params }: { params: { id: string } }) {
+  const {id} = params
   try {
-    const origin = req.headers.get("origin")
     const client = await clientPromise;
     if (!client) {
       throw new Error('Can not connect to database')
     }
-
+    
     const db = client.db("TodoApp");
     const collection = db.collection("TodoCollection")
     const updateTask = await req.json()
     const result = id === updateTask.id ? await collection.updateOne({ _id: new ObjectId(id) }, { $set: updateTask }) : null;
     const res = result?.modifiedCount
-    return NextResponse.json({ success: true, data: res }, {
-      status: 200, headers: {
-        'Access-Control-Allow-Origin': origin || '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    });
+    return NextResponse.json({ success: true, data: res }, { status: 200 ,headers:headers});
 
-  } catch (e: any) {
+  } catch (e:any) {
     const res = e.message
-    return NextResponse.json({ success: true, data: res }, {
-      status: 400, headers: {
-        'Access-Control-Allow-Origin': origin || '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    });
+    return NextResponse.json({ success: true, data: res }, { status: 400 ,headers:headers});
   }
 }
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-  const { id } = params
+export async function DELETE(req: Request,{ params }: { params: { id: string } }) {
+  const {id} = params
   try {
-    const origin = req.headers.get("origin")
     const client = await clientPromise;
     if (!client) {
       throw new Error('Can not connect to database')
     }
     const db = client.db("TodoApp");
     const collection = db.collection("TodoCollection")
-    const result = typeof id === "string" ? await collection.deleteOne({ _id: new ObjectId(id) }) : null
-    const response = result?.deletedCount
-    return NextResponse.json({ success: true, data: response }, {
-      status: 200, headers: {
-        'Access-Control-Allow-Origin': origin || '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    });
-  } catch (e: any) {
+    const result = typeof id === "string" ? await collection.deleteOne({ _id: new ObjectId(id) }) : null   
+    const response =  result?.deletedCount
+    return NextResponse.json({ success: true, data: response }, { status: 200 ,headers:headers});
+  } catch (e:any) {
     const message = e.message
-    return NextResponse.json({ success: false, data: message }, {
-      status: 400, headers: {
-        'Access-Control-Allow-Origin': origin || '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    });
+    return NextResponse.json({ success: false, data: message }, { status: 400 ,headers:headers});
   }
 }
--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,9 +1,15 @@
 import clientPromise from "@/database/mongodb";
 import { Task, TasksState } from "@/types/index";
 import { NextResponse } from "next/server";
+const  headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Access-Control-Allow-Credentials': 'true',
+}
 export async function GET(req: Request) {
   try {
-    const origin = req.headers.get("origin")
+    const origin=req.headers.get("origin")
     const client = await clientPromise;
     if (!client) {
       throw new Error('Can not connect to database')
@@ -14,49 +20,44 @@ export async function GET(req: Request) {
       .find({})
       .sort({ metacritic: -1 })
       .toArray();
-    const tasksWithId: any = result.map((task: any) => {
-      const { _id, ...rest } = task;
-      return { id: _id.toHexString(), ...rest };
-    });
+      const tasksWithId : any = result.map((task:any) => {
+        const { _id, ...rest } = task;
+        return { id: _id.toHexString(), ...rest };
+      });
 
-    const tasksState: TasksState = {
-      overdue: tasksWithId.filter((task: any) => {
-        return (task.status === "todo" || task.status === "doing") && new Date(task.deadline) <= new Date();
-      }),
-      todo: tasksWithId.filter((task: any) => {
-        return task.status === "todo" && new Date(task.deadline) > new Date();
-      }),
-      doing: tasksWithId.filter((task: any) => {
-        return task.status === "doing" && new Date(task.deadline) > new Date();
-      }),
-      done: tasksWithId.filter((task: any) => {
-        return task.status === "done";
-      }),
-    };
-
-    return NextResponse.json({ success: true, data: tasksState }, {
-      status: 200, headers: {
+      const tasksState: TasksState = {
+        overdue: tasksWithId.filter((task: any) => {
+          return (task.status === "todo" || task.status === "doing") && new Date(task.deadline) <= new Date();
+        }),
+        todo: tasksWithId.filter((task: any) => {
+          return task.status === "todo" && new Date(task.deadline) > new Date();
+        }),
+        doing: tasksWithId.filter((task: any) => {
+          return task.status === "doing" && new Date(task.deadline) > new Date();
+        }),
+        done: tasksWithId.filter((task: any) => {
+          return task.status === "done";
+        }),
+      };
+      
+      return NextResponse.json({ success: true, data: tasksState }, { status: 200, headers: {
         'Access-Control-Allow-Origin': origin || '*',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    });
-
-
+      }, });
+    
+      
   } catch (e) {
     console.log(e)
-    return NextResponse.json({ success: false, data: e }, {
-      status: 400, headers: {
-        'Access-Control-Allow-Origin': origin || '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    });
+    return NextResponse.json({ success: false, data: e }, { status: 400,headers: {
+      'Access-Control-Allow-Origin': origin || '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    }, });
   }
 }
 export async function POST(req: Request) {
   try {
-    const origin = req.headers.get("origin")
     const client = await clientPromise;
     if (!client) {
       throw new Error('Can not connect to database')
@@ -64,23 +65,11 @@ export async function POST(req: Request) {
     const db = client.db("TodoApp");
     const collection = db.collection("TodoCollection")
     const taskData = await req.json()
-    const { id, ...value } = taskData
+    const {id,...value} = taskData
     await collection.insertOne(value)
-    return NextResponse.json({ success: true, data: value._id }, {
-      status: 200, headers: {
-        'Access-Control-Allow-Origin': origin || '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    });
+    return NextResponse.json({ success: true, data: value._id }, { status: 200,headers:headers });
   } catch (e) {
     console.log(e)
-    return NextResponse.json({ success: true, data: e }, {
-      status: 200, headers: {
-        'Access-Control-Allow-Origin': origin || '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    });
+    return NextResponse.json({ success: true, data: e }, { status: 200,headers:headers });
   }
 }
