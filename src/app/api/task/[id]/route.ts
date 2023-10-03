1f521810fd3dec488fc061583d33ccc58c5668fe
phung <you@example.com>
2023-10-03 22:39:14 +0700

    update cors

--- src/app/api/task/[id]/route.ts
+++ src/app/api/task/[id]/route.ts
@@ -2,34 +2,46 @@ import clientPromise from "@/database/mongodb";
 import { Task } from "@/types/index";
 import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
-const  headers = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-  'Access-Control-Allow-Credentials': 'true',
-}
-export async function PUT(req: Request,{ params }: { params: { id: string } }) {
-  const {id} = params
+
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
+  const { id } = params
+  const origin = req.headers.get("origin")
+
   try {
     const client = await clientPromise;
     if (!client) {
       throw new Error('Can not connect to database')
     }
-    
+
     const db = client.db("TodoApp");
     const collection = db.collection("TodoCollection")
     const updateTask = await req.json()
     const result = id === updateTask.id ? await collection.updateOne({ _id: new ObjectId(id) }, { $set: updateTask }) : null;
     const res = result?.modifiedCount
-    return NextResponse.json({ success: true, data: res }, { status: 200 ,headers:headers});
+    return NextResponse.json({ success: true, data: res }, {
+      status: 200, headers: {
+        'Access-Control-Allow-Origin': origin || '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Credentials': 'true',
+      },
+    });
 
-  } catch (e:any) {
+  } catch (e: any) {
     const res = e.message
-    return NextResponse.json({ success: true, data: res }, { status: 400 ,headers:headers});
+    return NextResponse.json({ success: true, data: res }, {
+      status: 400, headers: {
+        'Access-Control-Allow-Origin': origin || '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Credentials': 'true',
+      },
+    });
   }
 }
-export async function DELETE(req: Request,{ params }: { params: { id: string } }) {
-  const {id} = params
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  const { id } = params
+  const origin = req.headers.get("origin")
   try {
     const client = await clientPromise;
     if (!client) {
@@ -37,11 +49,25 @@ export async function DELETE(req: Request,{ params }: { params: { id: string } }
     }
     const db = client.db("TodoApp");
     const collection = db.collection("TodoCollection")
-    const result = typeof id === "string" ? await collection.deleteOne({ _id: new ObjectId(id) }) : null   
-    const response =  result?.deletedCount
-    return NextResponse.json({ success: true, data: response }, { status: 200 ,headers:headers});
-  } catch (e:any) {
+    const result = typeof id === "string" ? await collection.deleteOne({ _id: new ObjectId(id) }) : null
+    const response = result?.deletedCount
+    return NextResponse.json({ success: true, data: response }, {
+      status: 200, headers: {
+        'Access-Control-Allow-Origin': origin || '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Credentials': 'true',
+      },
+    });
+  } catch (e: any) {
     const message = e.message
-    return NextResponse.json({ success: false, data: message }, { status: 400 ,headers:headers});
+    return NextResponse.json({ success: false, data: message }, {
+      status: 200, headers: {
+        'Access-Control-Allow-Origin': origin || '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Credentials': 'true',
+      },
+    });
   }
 }
