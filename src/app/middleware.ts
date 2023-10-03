93d321fa7486bc29e15f14aadacbeac700d0a8e6
phung <you@example.com>
2023-10-03 23:32:39 +0700

    update

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -4,18 +4,16 @@ export function middleware(request: NextRequest) {
     const origin =request.headers.get("origin")
 
     const res = NextResponse.next()
-    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Credentials', "false")
     res.headers.append('Access-Control-Allow-Origin', origin || '*')
     res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,POST,PUT')
-    res.headers.append(
-        'Access-Control-Allow-Headers',
+    res.headers.append('Access-Control-Allow-Headers',
         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
 
     return res
 }
 
-// specify the path regex to apply the middleware to
 export const config = {
     matcher: '/api/:path*',
 }
